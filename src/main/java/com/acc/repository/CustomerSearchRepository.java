465c140d5e28218c6f32b3784640afbff744ed2b
Raj Savaj <rsavaj7@gmail.com>
2025-07-16 12:40:35 +0530

    Customer search by ID and IC.

--- src/main/java/com/acc/repository/CustomerSearchRepository.java
+++ src/main/java/com/acc/repository/CustomerSearchRepository.java
@@ -32,4 +32,55 @@ public interface CustomerSearchRepository extends JpaRepository<CustomerEntity, 
         AND B.flg_mnt_status = 'A'
     """, nativeQuery = true)
     List<Object[]> searchByCustomerShortName(@Param("shortName") String shortName);
+
+    @Query(value = """
+    SELECT 
+        A.cod_cust_id AS customerId, 
+        A.nam_cust_full AS customerName, 
+        A.nam_cust_shrt AS customerShortName,
+        A.cod_cust_natl_id AS customerIC, 
+        A.flg_cust_typ AS type, 
+        B.txt_cust_typ AS category,
+        A.cod_cc_homebrn AS homeBranch, 
+        B.flg_typ_class AS typeClass,
+        TRIM(
+            COALESCE(A.txt_custadr_add1, '') || ' ' ||
+            COALESCE(A.txt_custadr_add2, '') || ' ' ||
+            COALESCE(A.txt_custadr_add3, '')
+        ) AS fullAddress
+    FROM 
+        ci_custmast A, 
+        ci_cust_types B
+    WHERE 
+        A.cod_cust_id = :customerId
+        AND A.flg_cust_typ = B.flg_cust_typ
+        AND B.flg_mnt_status = 'A'
+    """, nativeQuery = true)
+    List<Object[]> searchCustomerById(@Param("customerId") Long customerId);
+
+
+    @Query(value = """
+    SELECT 
+        A.cod_cust_id AS "Customer Id",
+        A.nam_cust_full AS "Customer Name",
+        A.nam_cust_shrt AS "Customer Short Name",
+        A.cod_cust_natl_id AS "Customer IC",
+        A.flg_cust_typ AS "Type",
+        B.txt_cust_typ AS "Category",
+        A.cod_cc_homebrn AS "Home Branch",
+        A.cod_cust_status || ':' || DECODE(NVL(A.cod_cust_status, 8), 7, 'Dormant', 'Regular') AS "Status",
+        B.flg_typ_class,
+        A.txt_custadr_add1,
+        A.txt_custadr_add2,
+        A.txt_custadr_add3
+    FROM 
+        ci_custmast A,
+        ci_cust_types B
+    WHERE 
+        A.cod_cust_natl_id = :searchString
+        AND A.flg_cust_typ = B.flg_cust_typ
+        AND B.flg_mnt_status = 'A'
+    """, nativeQuery = true)
+    List<Object[]> searchCustomerByCustomerIc(@Param("searchString") String searchString);
+
 }
--- src/main/java/com/acc/services/CustomerSearchService.java
+++ src/main/java/com/acc/services/CustomerSearchService.java
@@ -22,26 +22,35 @@ public class CustomerSearchService {
     private ChAcctMastRepo acctMastRepo;
 
     public List<CustomerSearchResponseDTO> searchCustomers(CustomerSearchRequest request) {
-            List<CustomerSearchResponseDTO> result = new ArrayList<>();
-            if ("Customer Short Name".equalsIgnoreCase(request.getSearchCriteria())) {
-                for (Object[] row : customerRepository.searchByCustomerShortName(request.getSearchString())) {
-                    result.add(new CustomerSearchResponseDTO(
-                            (String) row[0].toString(),
-                            (String) row[1].toString(),
-                            (String) row[2].toString(),
-                            (String) row[3].toString(),
-                            (String) row[4].toString(),
-                            (String) row[5].toString(),
-                            (String) row[6].toString(),
-                            (String) row[7].toString(),
-                            (String) row[8].toString()
-                    ));
-                }
-                return result;
+
+            List<CustomerSearchResponseDTO> customerDetailDto = new ArrayList<>();
+        List<Object[]> result = new ArrayList<>();
+        if(request.getSearchCriteria().equals("Customer Short Name")){
+            result = customerRepository.searchByCustomerShortName(request.getSearchString());
+        }
+        else  if(request.getSearchCriteria().equals("Customer ID")){
+            result = customerRepository.searchCustomerById(Long.parseLong(request.getSearchString()));
+        }
+        else if(request.getSearchCriteria().equals("Customer IC")){
+            result = customerRepository.searchCustomerByCustomerIc(request.getSearchString());
+        }
+        if (result != null)
+        {
+            for (Object[] row : result) {
+                customerDetailDto.add(new CustomerSearchResponseDTO(
+                        (String) row[0].toString(),
+                        (String) row[1].toString(),
+                        (String) row[2].toString(),
+                        (String) row[3].toString(),
+                        (String) row[4].toString(),
+                        (String) row[5].toString(),
+                        (String) row[6].toString(),
+                        (String) row[7].toString(),
+                        (String) row[8].toString()
+                ));
             }
-            else {
-                throw new IllegalArgumentException("Unsupported search criteria");
-            }
+        }
+                return customerDetailDto;
         }
 
     public List<CustomerAccountInfoDTO> getAccountInfo(String accountNo) {
